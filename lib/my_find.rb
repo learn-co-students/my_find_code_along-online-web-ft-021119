0fae1945d5dd15946001b541bb739313585c6c53
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 00:57:37 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  while i < collection.length
+    # This is the block that we are yielding to:
+    # {|i| i % 3 == 0 && i % 5 == 0 }
+    return (collection[i]) if yield(collection[i])
 
+    i += 1
+  end
 end
